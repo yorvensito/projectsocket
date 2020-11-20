fc9add84101a6360bed6ca889bbdf8545dbfbf91
George Alvarado <programador1@email.cloudnets.co>
2020-11-19 21:15:02 -0500

    Se agrego el main principal

--- main.js
+++ main.js
@@ -1,149 +1,155 @@
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var port = process.env.PORT || 5001;
+var WebSocketServer = require("ws").Server,
+  http = require("http"),
+  express = require("express"),
+  app = express(),
+  port = 5001;
 
+const axios = require("axios");
+const { json } = require("body-parser");
+const { response } = require("express");
 
-
-//carlos.ariza@safekeeping.com
-//Carlos-200
-//redislabs
-
-/* var redis = require('redis');
-var redisClient = redis.createClient({host : 'redis-13341.c1.us-west-2-2.ec2.cloud.redislabs.com', port : 13341}); */
-
-/* redisClient.auth('43Fq9hT5c6MEP6B7QGsjyq9PDPVMQBEc',function(err,reply) {
-	if(!err) {
-		console.log("Bien: Verificando la seguridad del sistema redis "+reply+" "+ Date());
-	}else{
-		console.log('Mal: Configure la seguridad del sistema redis  con > redi-cli.exe CONFIG SET requirepass "800ca200" '+err+' '+Date());
-	}
-}); */
-//redisClient.del("channelist_All",function(err,val){});
-
-/* redisClient.on('ready',function() {
-	console.log("Bien: Redis is ready... OK "+ Date());
-});
-
-redisClient.on('error',function() {
-	console.log("Mal: Error in Redis "+Date());
-});
- */
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   res.send('{"status":"200"}');
 });
-
 
 var server = http.createServer(app);
 
 server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
+var wss = new WebSocketServer({ server: server });
 
-var wss = new WebSocketServer({server: server})
-
-wss.options.maxPayload = 512 * 1024;
+wss.options.maxPayload = 64 * 1024;
 wss.options.server.timeout = 120000;
 wss.options.server.keepAliveTimeout = 5000;
 
-var tunneles = [];
-
-wss.broadcast = function broadcast(data,channel) {
+wss.broadcast = function broadcast(data, channel, id) {
   wss.clients.forEach(function each(client) {
-	if(client.channelTunnel == channel){
-		console.log(Date(),"se fue a: "+client.id+" con data "+data);
-		client.send(data);
-	}
+    if (client.channelTunnel == channel && client.id !== id) {
+      console.log(Date(), "se fue a: " + client.id + " con data " + data);
+      client.send(data);
+    }
   });
 };
 
-function verifyToken(token, callback){
-	var jwt = require('jsonwebtoken');
-	jwt.verify(token, 'clWve-G*-9)1', function(err, decoded) {
-		if(err){
-			return callback([false,false]);
-		}else{
-			return callback([true,decoded]);
-		}
-	});
+async function verifyToken(id_usuario, token, callback) {
+  try {
+    const res = await axios({
+      method: "POST",
+      url:
+        "https://dev.totalkme.com:26000/api/v1/core/usuarios/validate/token/socket/",
+      data: {
+        id_usuario: id_usuario,
+        token: token,
+      },
+    }).then((response) => response.data);
+    return callback([true, res]);
+  } catch (error) {
+    return callback([false, error]);
+  }
 }
+wss.list = [];
+wss.on("connection", function (ws, req, hed) {
+  var identy = "";
 
-wss.on("connection", function(ws,req,hed) {
-	
-	var identy = "";
+  if (req.rawHeaders.indexOf("Sec-Websocket-Key") > -1) {
+    identy = req.rawHeaders[req.rawHeaders.indexOf("Sec-Websocket-Key") + 1];
+  }
 
-	if(req.rawHeaders.indexOf("Sec-Websocket-Key")>-1){
-		identy = req.rawHeaders[req.rawHeaders.indexOf("Sec-Websocket-Key") + 1];
-	}
-	
-	if(req.rawHeaders.indexOf("Sec-WebSocket-Key")>-1){
-		identy = req.rawHeaders[req.rawHeaders.indexOf("Sec-WebSocket-Key") + 1];
-	}
-	
-	if(req.rawHeaders.indexOf("sec-websocket-key")>-1){
-		identy = req.rawHeaders[req.rawHeaders.indexOf("sec-websocket-key") + 1];
-	}
-	
-	
+  if (req.rawHeaders.indexOf("Sec-WebSocket-Key") > -1) {
+    identy = req.rawHeaders[req.rawHeaders.indexOf("Sec-WebSocket-Key") + 1];
+  }
 
-	ws.id = identy;
-	if(req.url.indexOf("/")>-1){
-		let valores = req.url.split("/");
-		if(valores.length==3){
-			let token = valores[1];
-			let channel = valores[2];
-			verifyToken(token,function(value){
-				if(value[0]!==false){
-					
-					ws.channelTunnel = channel;
-					ws.on('message', function incoming(data) {
-						
-						console.log(Date(),"llego de: "+identy);
-						console.log(Date(),"data es: "+data);
-						console.log(Date(),"channel es: "+channel);
-						
-						try{
-							var info = JSON.parse(data);
-							
-							if(info.type == "new"){
-								
-								redisClient.get("channelist_"+channel,function(err,val){
-									if(val==null){
-										redisClient.set("channelist_"+channel,JSON.stringify([{"id":info.data.id,"name":identy,"media":info.data.media,"channel":channel}]),function(err,val){
-											wss.broadcast(JSON.stringify({"type":"peers","data":[{"id":info.data.id,"name":identy,"media":info.data.media,"channel":channel}]}),channel);
-										});
-									}else{
-										var nuevo = JSON.parse(val);
-										
-										function filtro(valores){
-											return valores.id == info.data.id;
-										}
-										var resul = nuevo.find(filtro);
-										if(resul==undefined && val.indexOf(identy)==-1){
-											nuevo.push({"id":info.data.id,"name":identy,"media":info.data.media,"channel":channel});
-											redisClient.set("channelist_"+channel,JSON.stringify(nuevo),function(err,val){
-												wss.broadcast(JSON.stringify({"type":"peers","data":nuevo}),channel);
-											});
-										}else{
-											wss.broadcast(JSON.stringify({"type":"peers","data":nuevo}),channel);
-										}
-									}
-								});
-								
-							}else{
-								wss.broadcast(data,channel);
-							}
-						}catch(e){
-							console.log(e);
-						}
-					});
-					
-				}
-			})
-		}
-	}
-})
+  if (req.rawHeaders.indexOf("sec-websocket-key") > -1) {
+    identy = req.rawHeaders[req.rawHeaders.indexOf("sec-websocket-key") + 1];
+  }
 
-	//ws://localhost:5001/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNTc4MDkyZDhhMDE4NjBlMmVlOThjOWQ4MWM4NjA1ZTczNmRlZWI3NDczNWIzZDJjZDZhYmMwZGZhMzdiZTRlMCIsImlhdCI6MTYwMTc0OTk1MiwiZXhwIjoxNjA2OTMzOTUyfQ.JRQeuob-I83ZSMKEwyR0iqeykrdPeexZwTiSrnbl4hI
+  ws.id = identy;
+  if (req.url.indexOf("/") > -1) {
+    let valores = req.url.split("/");
+    if (valores.length == 4 || valores.length == 3) {
+      let token = valores[1];
+      let user_id = valores[2];
+      ws.id_user = user_id;
+      //   validar el usuario en la api
+      verifyToken(user_id, token, function (result) {
+        console.log(result);
+        if (result[0] === true) {
+          if (/^[a-zA-Z0-9]{40}$/.test(token) === true) {
+            if (valores[3] !== undefined) {
+              let channel = valores[3];
+              //verifyToken(token, function (value) {
+              //if (value[0] !== false) {
+              ws.channelTunnel = channel;
+              ws.on("message", function incoming(data) {
+                console.log(Date(), "llego de: " + identy);
+                console.log(Date(), "data es: " + data);
+                console.log(Date(), "channel es: " + channel);
+
+                console.log("es este ", wss.list);
+
+                try {
+                  let info = JSON.parse(data);
+                  if (info.reconnect == true) {
+                    //{"reconnect":true}
+                    function filtro(valores) {
+                      return valores.channel == channel;
+                    }
+                    let result = wss.list.find(filtro);
+                    if (result !== undefined) {
+                      if (result.from.indexOf(user_id) == -1) {
+                        ws.send(
+                          JSON.stringify({
+                            confirm: true,
+                            operations: result.data,
+                          })
+                        );
+                      } else {
+                        ws.send(JSON.stringify({ confirm: true }));
+                      }
+                    }
+                  } else if (info.response == true) {
+                    //{"response":true}
+                    wss.list.find(function filtro(valores, index) {
+                      if (valores.channel == channel) {
+                        wss.list[index].from.push(user_id);
+                        console.log(wss.list[index].from);
+                        if (wss.list[index].from.length == 3) {
+                          wss.list.splice(index, 1);
+                        }
+                        ws.send(JSON.stringify({ confirm: true }));
+                      }
+                    });
+                  } else if (info.datas == true) {
+                    //{"datas":true,"info":{"x":"y"}}
+                    wss.list.push({
+                      channel: channel,
+                      data: info.info,
+                      from: [user_id],
+                    });
+                    ws.send(JSON.stringify({ confirm: true }));
+                    wss.broadcast(data, channel, identy);
+                  }
+                } catch (e) {
+                  ws.send(JSON.stringify({ error: "not json" }));
+                }
+              });
+            } else {
+              ws.on("message", function incoming(data) {
+                try {
+                  let info = JSON.parse(data); // {"order":"cloud","id":"123456","type":"2"}
+                  if (info.order == true) {
+                    //el partner = 123456 y si es mi id uso el order como channel
+                    wss.broadcast(data, channel, identy);
+                  }
+                } catch (e) {
+                  ws.send(JSON.stringify({ error: "not json" }));
+                }
+              });
+            }
+          }
+        }
+      });
+    }
+  }
+});

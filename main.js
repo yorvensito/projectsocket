e9cb0587ddd505b7dfda927fd2c61d14b6271f91
George Alvarado <programador1@email.cloudnets.co>
2020-12-09 09:20:25 -0500

    Se agrego el nivel 0

--- main.js
+++ main.js
@@ -73,92 +73,98 @@ wss.on("connection", function (ws, req, hed) {
   ws.id = identy;
   if (req.url.indexOf("/") > -1) {
     let valores = req.url.split("/");
-    if (valores.length == 4 || valores.length == 3) {
+    if (valores.length == 4 || valores.length == 3 || valores.length == 2) {
       let token = valores[1];
       let user_id = valores[2];
       ws.id_user = user_id;
       //   validar el usuario en la api
-      verifyToken(user_id, token, function (result) {
-        if (result[0] === true) {
-          if (/^[a-zA-Z0-9]{40}$/.test(token) === true) {
-            if (valores[3] !== undefined) {
-              let channel = valores[3];
-              //verifyToken(token, function (value) {
-              //if (value[0] !== false) {
-              ws.channelTunnel = channel;
-              ws.on("message", function incoming(data) {
-                console.log(Date(), "llego de: " + identy);
-                console.log(Date(), "data es: " + data);
-                console.log(Date(), "channel es: " + channel);
+      if (valores.length == 2) {
+        ws.on("message", function incoming(data) {
+          wss.broadcast(data); //ws://localhost:5001/invoque
+        });
+      } else {
+        verifyToken(user_id, token, function (result) {
+          if (result[0] === true) {
+            if (/^[a-zA-Z0-9]{40}$/.test(token) === true) {
+              if (valores[3] !== undefined) {
+                let channel = valores[3];
+                //verifyToken(token, function (value) {
+                //if (value[0] !== false) {
+                ws.channelTunnel = channel;
+                ws.on("message", function incoming(data) {
+                  console.log(Date(), "llego de: " + identy);
+                  console.log(Date(), "data es: " + data);
+                  console.log(Date(), "channel es: " + channel);
 
-                console.log("es este ", wss.list);
+                  console.log("es este ", wss.list);
 
-                try {
-                  let info = JSON.parse(data);
-                  if (info.reconnect == true) {
-                    //{"reconnect":true}
-                    function filtro(valores) {
-                      return valores.channel == channel;
-                    }
-                    let result = wss.list.find(filtro);
-                    if (result !== undefined) {
-                      if (result.from.indexOf(user_id) == -1) {
-                        ws.send(
-                          JSON.stringify({
-                            confirm: true,
-                            operations: result.data,
-                          })
-                        );
-                      } else {
-                        ws.send(JSON.stringify({ confirm: true }));
+                  try {
+                    let info = JSON.parse(data);
+                    if (info.reconnect == true) {
+                      //{"reconnect":true}
+                      function filtro(valores) {
+                        return valores.channel == channel;
                       }
-                    }
-                  } else if (info.response == true) {
-                    //{"response":true}
-                    wss.list.find(function filtro(valores, index) {
-                      if (valores.channel == channel) {
-                        wss.list[index].from.push(user_id);
-                        console.log(wss.list[index].from);
-                        if (wss.list[index].from.length == 3) {
-                          wss.list.splice(index, 1);
+                      let result = wss.list.find(filtro);
+                      if (result !== undefined) {
+                        if (result.from.indexOf(user_id) == -1) {
+                          ws.send(
+                            JSON.stringify({
+                              confirm: true,
+                              operations: result.data,
+                            })
+                          );
+                        } else {
+                          ws.send(JSON.stringify({ confirm: true }));
                         }
-                        ws.send(JSON.stringify({ confirm: true }));
                       }
-                    });
-                  } else if (info.datas == true) {
-                    //{"datas":true,"info":{"x":"y"}}
-                    wss.list.push({
-                      channel: channel,
-                      data: info.info,
-                      from: [user_id],
-                    });
-                    ws.send(JSON.stringify({ confirm: true }));
-                    wss.broadcast(data, channel, identy);
+                    } else if (info.response == true) {
+                      //{"response":true}
+                      wss.list.find(function filtro(valores, index) {
+                        if (valores.channel == channel) {
+                          wss.list[index].from.push(user_id);
+                          console.log(wss.list[index].from);
+                          if (wss.list[index].from.length == 3) {
+                            wss.list.splice(index, 1);
+                          }
+                          ws.send(JSON.stringify({ confirm: true }));
+                        }
+                      });
+                    } else if (info.datas == true) {
+                      //{"datas":true,"info":{"x":"y"}}
+                      wss.list.push({
+                        channel: channel,
+                        data: info.info,
+                        from: [user_id],
+                      });
+                      ws.send(JSON.stringify({ confirm: true }));
+                      wss.broadcast(data, channel, identy);
+                    }
+                  } catch (e) {
+                    ws.send(JSON.stringify({ error: "not json" }));
                   }
-                } catch (e) {
-                  ws.send(JSON.stringify({ error: "not json" }));
-                }
-              });
-            } else {
-              ws.on("message", function incoming(data) {
-                console.log(">>>>>>>>>>>>>>>>>>>>>>>", Date());
-                console.log(data);
-                console.log("<<<<<<<<<<<<<<<<<<<<<<<", Date());
+                });
+              } else {
+                ws.on("message", function incoming(data) {
+                  console.log(">>>>>>>>>>>>>>>>>>>>>>>", Date());
+                  console.log(data);
+                  console.log("<<<<<<<<<<<<<<<<<<<<<<<", Date());
 
-                try {
-                  let info = JSON.parse(data); // {"order":true,"id":"123456","type":"2", "order_id": 123555}
-                  if (info.order == true || info.gestion == true) {
-                    //el partner = 123456 y si es mi id uso el order como channel
-                    wss.broadcast(data, null, identy);
+                  try {
+                    let info = JSON.parse(data); // {"order":true,"id":"123456","type":"2", "order_id": 123555}
+                    if (info.order == true || info.gestion == true) {
+                      //el partner = 123456 y si es mi id uso el order como channel
+                      wss.broadcast(data, null, identy);
+                    }
+                  } catch (e) {
+                    ws.send(JSON.stringify({ error: "not json" }));
                   }
-                } catch (e) {
-                  ws.send(JSON.stringify({ error: "not json" }));
-                }
-              });
+                });
+              }
             }
           }
-        }
-      });
+        });
+      }
     } else {
       ws.close();
     }
